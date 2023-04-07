20e6cf2ab4e1055c238bb8c325e6284c5555998b
Thomas Vass <thomasstefanvass@gmail.com>
2023-04-07 15:35:22 -0400

    initial commit

--- client/index.js
+++ client/index.js
@@ -4,14 +4,34 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const arguments = process.argv.slice(2);
+const name = arguments.join(' ') 
+
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+      if (!name){console.log("You need to provide your name: node index.js <your-name>")}else{
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      const merkleTree = new MerkleTree(niceList);
+
+      const index = niceList.findIndex(n => n === name);
+      const proof = merkleTree.getProof(index);
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {name, proof})
     // TODO: add request body parameters here!
-  });
-
-  console.log({ gift });
+  
+   if (gift=="You got a toy robot!"){
+    console.log(`
+    _________________
+ |'-========OoO===='-.
+ | ||'-.____'-.'-.____'-.          __
+ | ||  |      |  |      |         (**\\ Go ahead!
+  '-|  |      |  |      |         -.' \\  Open it!
+     '-|______|__|______|           , .\`
+                                 /__|
+                                // ||
+                               -'  -'
+${gift}
+`)}else{console.log(gift)};
+}
 }
 
-main();
+main(name);
